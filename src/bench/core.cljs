c0f18bfe264bb5741c5728b707bf68b9695ac305
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-05-06 12:50:36 +0100

    Fleshed out the front end

--- src/bench/core.cljs
+++ src/bench/core.cljs
@@ -12,65 +12,82 @@
 (def f2 #(+ 1 %))
 
 (defonce app-state (reagent/atom {:fastest nil
-                                  :results {}}))
+                                  :running? false
+                                  :results []}))
 
 (defn run-bench
   []
   (let [suite (Suite.)]
     (swap! app-state (fn [xs]
-                       (merge xs {:results {}
+                       (merge xs {:results []
+                                  :running? true
                                   :fastest nil})))
     (.. suite
-        (add "partial"
+        (add "partial function (partial + 1)"
              (fn []
                (f1 1)))
-        (add "anonymous function"
+        (add "anonymous function #(+ 1 %)"
              (fn []
                (f2 1)))
         (on "cycle" (fn [event]
                       (let [b    (-> event .-target)
                             stat (-> b .-stats)]
-                        (swap! app-state assoc-in [:results (.-name b)] stat))))
+                        (swap! app-state update :results  conj [(.-name b) stat]))))
         (on "complete" (fn []
                          (this-as this
                            (let [fastest  (-> this
                                               (.filter "fastest")
                                               (.map "name")
                                               (aget 0))]
-                             (swap! app-state assoc :fastest fastest)))))
+                             (swap! app-state merge {:fastest fastest
+                                                     :running? false})))))
         (run #js {"async" true}))))
+
+(defn create-row
+  [header n results fastest]
+  [:tr
+   [:td header]
+   (for [[k v] results]
+     [:td {:key k
+           :style (when (= k fastest)
+                    {:background-color "#d9edf7"})}
+      (gobj/get v n)])])
 
 (defn main-page
   []
   (let [data         @app-state
-        series-names (sort (keys (:results data)))]
+        series-names (map first (:results data))
+        running?     (:running? data)
+        fastest      (:fastest data) ]
     [:div
-     [:button {:onClick #(run-bench)} "Run Benchmark"]
-     [:h2 (str "Fastest function: " (:fastest data))]
+     [:h1 "Benchmarking Clojurescript code"]
+     [:button.btn.btn-primary {:onClick (fn []
+                                          (when-not running?
+                                            (run-bench)))
+                               :disabled (when running?
+                                           "disabled")}
+      "Run Benchmark"]
+     (when running?
+       [:h3 "Benchmarks are running"])
      (when (seq series-names)
-       [:table
-        [:thead
-         [:tr
-          [:th ""]
-          (for [h series-names]
-            [:th {:key h} h])]]
-        [:tbody
-         [:tr
-          [:td "Mean"]
-          (for [h series-names]
-            [:td {:key h} (gobj/get (get-in data [:results h]) "mean")])]
-         [:tr
-          [:td "Deviation"]
-          (for [h series-names]
-            [:td {:key h} (gobj/get (get-in data [:results h]) "deviation")])]
-         [:tr
-          [:td "Margin of error"]
-          (for [h series-names]
-            [:td {:key h} (gobj/get (get-in data [:results h]) "moe")])]
-         [:tr
-          [:td "Relative moe"]
-          (for [h series-names]
-            [:td {:key h} (gobj/get (get-in data [:results h]) "rme")])]]])]))
+       [:div
+        [:h3 (str "Fastest function: " (:fastest data))]
+        [:table.table
+         [:thead
+          [:tr
+           [:th ""]
+           (for [h series-names]
+             [:th {:key h
+                   :style (when (= h fastest)
+                            {:background-color "#d9edf7"})} h])]]
+         [:tbody
+          [create-row "Mean" "mean" (:results data) fastest]
+          [create-row "Deviation" "deviation" (:results data) fastest]
+          [create-row "Margin of error" "moe" (:results data) fastest]
+          [create-row "Relative moe" "rme" (:results data) fastest]
+          [create-row "Standard moe" "sem" (:results data) fastest]]]]
+
+       )]))
 
 (defn mount-root
   []
